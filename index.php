42d5bfb42de6c15e5b7fb629563d3fa913e70c7d
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-04-16 20:56:06 +0300

    includes

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 	<link type="text/css" rel="stylesheet" href="assets/css/main.css" />
+	<link type="text/css" rel="stylesheet" href="assets/css/style-dg.css" />
 	<link type="text/css" rel="stylesheet" href="assets/css/jquery.countdown.css" />
 	<link type="text/css" rel="stylesheet" href="assets/css/lightbox.css" />
 
