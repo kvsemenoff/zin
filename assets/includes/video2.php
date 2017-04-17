6201f9140426bee5dae525a1ee749a7d95366ad2
Berzeg07 <berezg86@gmail.com>
2017-04-17 13:38:44 +0400

    29.14

--- assets/includes/video2.php
+++ assets/includes/video2.php
@@ -9,7 +9,7 @@
                             <p class="name"><span>Михаил и Дарья</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204229485?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204229485?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -17,7 +17,7 @@
                             <p class="name"><span>Алексей и Екатерина</span>Свадебный Клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202619094?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202619094?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -25,7 +25,7 @@
                             <p class="name"><span>Алексей и Екатерина</span> Love Story</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618374?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618374?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -33,7 +33,7 @@
                             <p class="name"><span>Анастасия и Антон</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202617432?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202617432?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -41,7 +41,7 @@
                             <p class="name"><span>Денис и Даша</span>Love Story</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202625395?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202625395?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -49,7 +49,7 @@
                             <p class="name"><span>Алексей и Екатерина</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202620688?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202620688?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -57,7 +57,7 @@
                             <p class="name"><span>Марат Башаров и Екатерина Архарова</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618754?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618754?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                    <div class="item ">
@@ -65,7 +65,7 @@
                             <p class="name"><span>Михаил и Кристина</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204231637?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204231637?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                    <!--div class="item ">
@@ -73,7 +73,7 @@
                             <p class="name"><span>Николай и Наталья</span><br></p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/91753372?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/91753372?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -81,7 +81,7 @@
                             <p class="name"><span>Осенняя прогулка</span><br></p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/62054489?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/62054489?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div-->
                   <div class="item ">
@@ -89,7 +89,7 @@
                             <p class="name"><span>Кирилл и Нина</span>Love Story</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202627011?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202627011?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -97,7 +97,7 @@
                             <p class="name"><span>Константин и Мария</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204218501?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204218501?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                   <div class="item ">
@@ -105,7 +105,7 @@
                             <p class="name"><span>Денис и Мария</span>Свадебный клип</p>
                       </div>
                       <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202621362?title=0&amp;byline=0&amp;portrait=0" width="422px" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202621362?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                       </div>
                   </div>
                 </div>
