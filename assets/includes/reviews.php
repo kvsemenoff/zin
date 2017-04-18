6b173aba310b033833150d0f5fb6844dd9e641a9
Berzeg07 <berezg86@gmail.com>
2017-04-18 17:00:11 +0400

    29.14

--- assets/includes/reviews.php
+++ assets/includes/reviews.php
@@ -3,122 +3,122 @@
     <h1 class="large">Отзывы</h1>
     <div class="box">
       <div class="row">
-        <div class="col-6">
+      <div class="col-md-6">
           <div class="video">
             <div class="inside_video">
               <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204229485?title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            </div>
           </div>
-          <div class="box_photo">
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/1/1.1.jpg" data-lightbox="image1">
-                  <img src="assets/images/pictures/image_reviews/1/1.1.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/1/1.2.jpg" data-lightbox="image1">
-                  <img src="assets/images/pictures/image_reviews/1/1.2.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/1/1.3.jpg" data-lightbox="image1">
-                  <img src="assets/images/pictures/image_reviews/1/1.3.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-6">
-          <p class="name"><span>Михаил и Дарья</span>Ребята так точно передали эмоции нашего дня и учли все все наши пожелания! </p>
-          <p class="text"><b>Дарья</b>: Дорогие Дима и Илья! Вы, наиталантлевейшие ребята, сотворили невозможную красоту. Так точно передали эмоции нашего дня, учли все все наши пожелания, помогали и были совершенно незаметными ниндзя! Фильм и ролик получились просто чудесными. Еще миллион раз готова сказать вам спасибо! Вы правда лучшие! Студия Zinoveev Brothers forever! </p>
-        </div>
       </div>
-    </div>
-    <div class="box">
-      <div class="row">
-        <div class="col-6">
-          <div class="video">
-            <div class="inside_video">
-              <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/205370553?title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            </div>
-          </div>
-          <div class="box_photo">
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/1/1.jpg" data-lightbox="image2">
-                  <img src="assets/images/pictures/image_reviews/1/1.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/1/2.jpg" data-lightbox="image2">
-                  <img src="assets/images/pictures/image_reviews/1/2.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/1/3.jpg" data-lightbox="image2">
-                  <img src="assets/images/pictures/image_reviews/1/3.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-6">
-          <p class="name"><span>Михаил и Юлия</span>Ты можешь вести себя естественно и полностью погрузиться в эмоции такого важного свадебного дня!</p>
-          <p class="text"><b>Юлия</b>: К выбору видеооператора на свадьбу мы подходили очень ответственно, хотелось запечатлеть этот день во всей красе. Очень рады, что решили сотрудничать с Дмитрием, он все рассказал, объяснил, что будет лучше смотреться, в итоге мы выбрали две камеры и не пожалели, видео очень динамичное. Качество съемки также на высоте. Еще один немаловажный плюс &#8211; оператора в работе практически не замечаешь, он не мешает, не отвлекает, просто делает свою работу, пока ты можешь вести себя естественно и полностью погрузиться в эмоции такого важного свадебного дня! Честно говоря, я до сих пор со слезами радости пересматриваю ролик, такую бурю эмоций он во мне вызывает) Так что мы всем довольны! Большое спасибо Диме и его команде за то, что помогли нам так красиво сохранить в памяти этот важный для нашей семьи день!</p>
-        </div>
+      <div class="box_photo">
+        <div class="photo">
+          <div class="inside_photo">
+            <a href="assets/images/pictures/image_reviews/1/1.1.jpg" data-lightbox="image1">
+              <img src="assets/images/pictures/image_reviews/1/1.1.jpg" alt="image review" />
+          </a>
       </div>
-    </div>
-    <div class="box">
-      <div class="row">
-        <div class="col-6">
-          <div class="video">
-            <div class="inside_video">
-                <object width="100%" height="100%">
-                    <param name="allowfullscreen" value="true" />
-                    <param name="allowscriptaccess" value="always" />
-                    <param onclick="_gaq.push(['_trackEvent', 'video5', 'play']);" name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=202627011&amp;force_embed=1&amp;server=vimeo.com&amp;title=0&amp;byline=0&amp;portrait=0&amp;" />
-                    <embed src="http://vimeo.com/moogaloop.swf?clip_id=202627011&amp;force_embed=1&amp;server=vimeo.com&amp;title=0&amp;byline=0&amp;portrait=0&amp;" type="application/x-shockwave-flash" allowfullscreen="false" allowscriptaccess="always" width="100%" height="100%"></embed>
-                              </object>
-            </div>
-          </div>
-          <div class="box_photo">
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/3/1.jpg" data-lightbox="image3">
-                  <img src="assets/images/pictures/image_reviews/3/1.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/3/2.jpg" data-lightbox="image3">
-                  <img src="assets/images/pictures/image_reviews/3/2.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-            <div class="photo">
-              <div class="inside_photo">
-                <a href="assets/images/pictures/image_reviews/3/3.jpg" data-lightbox="image3">
-                  <img src="assets/images/pictures/image_reviews/3/3.jpg" alt="image review" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-6">
-          <p class="name"><span>Кирилл и Нина</span>Love Story</p>
-          <p class="text"><b>Нина</b>: Мы бы хотели сказать большое спасибо Диме и Рите за то, что сняли для нас замечательное лавстори. Оно было в какой-то степени пророческим. Нам очень понравилось, что у ребят подход творческий, и мы не чувствовали себя во время съемки натянуто. Я до сих пор вспоминаю этот день, потому что мы действительно прикольно провели время, очень весело: делали снеговика, играли в снежки, на самом деле это было очень здорово, тем более, что остались об этом такие воспоминания.<br /><b>Кирилл</b>: Чувствуется, что ребята &#8211; профессионалы своего дела, очень грамотно подходят к каждому человеку. Я как, наверно, любой мужчина вообще не люблю сниматься, но с ребятами это было легко и комфортно. Это была просто замечательная прогулка, причем когда проводишь съемку иногда делаешь вещи, которые ты бы в принципе делать не стал. Хочется дурить, чудить, всегда они подскажут что делать, а что самое главное, никогда не знаешь, что и этого получится. Тебя снимают, не понятно как, а в итоге получается замечательный видеоролик, который останется в памяти на всю жизнь. Спасибо вам большое!</p>
-        </div>
-      </div>
-    </div>
   </div>
+  <div class="photo">
+      <div class="inside_photo">
+        <a href="assets/images/pictures/image_reviews/1/1.2.jpg" data-lightbox="image1">
+          <img src="assets/images/pictures/image_reviews/1/1.2.jpg" alt="image review" />
+      </a>
+  </div>
+</div>
+<div class="photo">
+  <div class="inside_photo">
+    <a href="assets/images/pictures/image_reviews/1/1.3.jpg" data-lightbox="image1">
+      <img src="assets/images/pictures/image_reviews/1/1.3.jpg" alt="image review" />
+  </a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+  <p class="name"><span>Михаил и Дарья</span>Ребята так точно передали эмоции нашего дня и учли все все наши пожелания! </p>
+  <p class="text"><b>Дарья</b>: Дорогие Дима и Илья! Вы, наиталантлевейшие ребята, сотворили невозможную красоту. Так точно передали эмоции нашего дня, учли все все наши пожелания, помогали и были совершенно незаметными ниндзя! Фильм и ролик получились просто чудесными. Еще миллион раз готова сказать вам спасибо! Вы правда лучшие! Студия Zinoveev Brothers forever! </p>
+</div>
+</div>
+</div>
+<div class="box">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="video">
+        <div class="inside_video">
+          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/205370553?title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+  </div>
+  <div class="box_photo">
+    <div class="photo">
+      <div class="inside_photo">
+        <a href="assets/images/pictures/image_reviews/1/1.jpg" data-lightbox="image2">
+          <img src="assets/images/pictures/image_reviews/1/1.jpg" alt="image review" />
+      </a>
+  </div>
+</div>
+<div class="photo">
+  <div class="inside_photo">
+    <a href="assets/images/pictures/image_reviews/1/2.jpg" data-lightbox="image2">
+      <img src="assets/images/pictures/image_reviews/1/2.jpg" alt="image review" />
+  </a>
+</div>
+</div>
+<div class="photo">
+  <div class="inside_photo">
+    <a href="assets/images/pictures/image_reviews/1/3.jpg" data-lightbox="image2">
+      <img src="assets/images/pictures/image_reviews/1/3.jpg" alt="image review" />
+  </a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+  <p class="name"><span>Михаил и Юлия</span>Ты можешь вести себя естественно и полностью погрузиться в эмоции такого важного свадебного дня!</p>
+  <p class="text"><b>Юлия</b>: К выбору видеооператора на свадьбу мы подходили очень ответственно, хотелось запечатлеть этот день во всей красе. Очень рады, что решили сотрудничать с Дмитрием, он все рассказал, объяснил, что будет лучше смотреться, в итоге мы выбрали две камеры и не пожалели, видео очень динамичное. Качество съемки также на высоте. Еще один немаловажный плюс &#8211; оператора в работе практически не замечаешь, он не мешает, не отвлекает, просто делает свою работу, пока ты можешь вести себя естественно и полностью погрузиться в эмоции такого важного свадебного дня! Честно говоря, я до сих пор со слезами радости пересматриваю ролик, такую бурю эмоций он во мне вызывает) Так что мы всем довольны! Большое спасибо Диме и его команде за то, что помогли нам так красиво сохранить в памяти этот важный для нашей семьи день!</p>
+</div>
+</div>
+</div>
+<div class="box">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="video">
+        <div class="inside_video">
+            <object width="100%" height="100%">
+                <param name="allowfullscreen" value="true" />
+                <param name="allowscriptaccess" value="always" />
+                <param onclick="_gaq.push(['_trackEvent', 'video5', 'play']);" name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=202627011&amp;force_embed=1&amp;server=vimeo.com&amp;title=0&amp;byline=0&amp;portrait=0&amp;" />
+                <embed src="http://vimeo.com/moogaloop.swf?clip_id=202627011&amp;force_embed=1&amp;server=vimeo.com&amp;title=0&amp;byline=0&amp;portrait=0&amp;" type="application/x-shockwave-flash" allowfullscreen="false" allowscriptaccess="always" width="100%" height="100%"></embed>
+            </object>
+        </div>
+    </div>
+    <div class="box_photo">
+        <div class="photo">
+          <div class="inside_photo">
+            <a href="assets/images/pictures/image_reviews/3/1.jpg" data-lightbox="image3">
+              <img src="assets/images/pictures/image_reviews/3/1.jpg" alt="image review" />
+          </a>
+      </div>
+  </div>
+  <div class="photo">
+      <div class="inside_photo">
+        <a href="assets/images/pictures/image_reviews/3/2.jpg" data-lightbox="image3">
+          <img src="assets/images/pictures/image_reviews/3/2.jpg" alt="image review" />
+      </a>
+  </div>
+</div>
+<div class="photo">
+  <div class="inside_photo">
+    <a href="assets/images/pictures/image_reviews/3/3.jpg" data-lightbox="image3">
+      <img src="assets/images/pictures/image_reviews/3/3.jpg" alt="image review" />
+  </a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+  <p class="name"><span>Кирилл и Нина</span>Love Story</p>
+  <p class="text"><b>Нина</b>: Мы бы хотели сказать большое спасибо Диме и Рите за то, что сняли для нас замечательное лавстори. Оно было в какой-то степени пророческим. Нам очень понравилось, что у ребят подход творческий, и мы не чувствовали себя во время съемки натянуто. Я до сих пор вспоминаю этот день, потому что мы действительно прикольно провели время, очень весело: делали снеговика, играли в снежки, на самом деле это было очень здорово, тем более, что остались об этом такие воспоминания.<br /><b>Кирилл</b>: Чувствуется, что ребята &#8211; профессионалы своего дела, очень грамотно подходят к каждому человеку. Я как, наверно, любой мужчина вообще не люблю сниматься, но с ребятами это было легко и комфортно. Это была просто замечательная прогулка, причем когда проводишь съемку иногда делаешь вещи, которые ты бы в принципе делать не стал. Хочется дурить, чудить, всегда они подскажут что делать, а что самое главное, никогда не знаешь, что и этого получится. Тебя снимают, не понятно как, а в итоге получается замечательный видеоролик, который останется в памяти на всю жизнь. Спасибо вам большое!</p>
+</div>
+</div>
+</div>
+</div>
 </div>
