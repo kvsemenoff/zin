4a753d96e1f4c9ff7ccd552e2781699c23db763c
Berzeg07 <berezg86@gmail.com>
2017-04-18 15:33:46 +0400

    29.14

--- assets/includes/video2.php
+++ assets/includes/video2.php
@@ -1,73 +1,73 @@
 <div id="video_2">
   <div class="content2">
     <h1 class="title">Мы не будем рассказывать про заоблачную<br />стоимость нашей профессиональной техники.<br /><b>Лучше оцените результат сами!</b></h1>
-          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                  <div class="item active">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Михаил и Дарья</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204229485?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Алексей и Екатерина</span>Свадебный Клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202619094?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Алексей и Екатерина</span> Love Story</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618374?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Анастасия и Антон</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202617432?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Денис и Даша</span>Love Story</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202625395?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Алексей и Екатерина</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202620688?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Марат Башаров и Екатерина Архарова</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618754?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                   <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Михаил и Кристина</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204231637?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+              <div id="car_capt" class="carousel-caption">
+                <p class="name"><span>Михаил и Дарья</span>Свадебный клип</p>
+            </div>
+            <div class="video_boxesss">
+              <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204229485?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+      </div>
+      <div class="item ">
+          <div id="car_capt" class="carousel-caption">
+            <p class="name"><span>Алексей и Екатерина</span>Свадебный Клип</p>
+        </div>
+        <div class="video_boxesss">
+          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202619094?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+  </div>
+  <div class="item ">
+      <div id="car_capt" class="carousel-caption">
+        <p class="name"><span>Алексей и Екатерина</span> Love Story</p>
+    </div>
+    <div class="video_boxesss">
+      <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618374?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  </div>
+</div>
+<div class="item ">
+  <div id="car_capt" class="carousel-caption">
+    <p class="name"><span>Анастасия и Антон</span>Свадебный клип</p>
+</div>
+<div class="video_boxesss">
+  <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202617432?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+</div>
+<div class="item ">
+  <div id="car_capt" class="carousel-caption">
+    <p class="name"><span>Денис и Даша</span>Love Story</p>
+</div>
+<div class="video_boxesss">
+  <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202625395?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+</div>
+<div class="item ">
+  <div id="car_capt" class="carousel-caption">
+    <p class="name"><span>Алексей и Екатерина</span>Свадебный клип</p>
+</div>
+<div class="video_boxesss">
+  <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202620688?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+</div>
+<div class="item ">
+  <div id="car_capt" class="carousel-caption">
+    <p class="name"><span>Марат Башаров и Екатерина Архарова</span>Свадебный клип</p>
+</div>
+<div class="video_boxesss">
+  <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202618754?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+</div>
+<div class="item ">
+  <div id="car_capt" class="carousel-caption">
+    <p class="name"><span>Михаил и Кристина</span>Свадебный клип</p>
+</div>
+<div class="video_boxesss">
+  <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204231637?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+</div>
                    <!--div class="item ">
                       <div class="carousel-caption">
                             <p class="name"><span>Николай и Наталья</span><br></p>
@@ -85,37 +85,37 @@
                       </div>
                   </div-->
                   <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Кирилл и Нина</span>Love Story</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202627011?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
+                      <div id="car_capt" class="carousel-caption">
+                        <p class="name"><span>Кирилл и Нина</span>Love Story</p>
+                    </div>
+                    <div class="video_boxesss">
+                      <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202627011?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                   </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Константин и Мария</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204218501?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
-                  <div class="item ">
-                      <div class="carousel-caption">
-                            <p class="name"><span>Денис и Мария</span>Свадебный клип</p>
-                      </div>
-                      <div class="video_boxesss">
-                          <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202621362?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                      </div>
-                  </div>
+              </div>
+              <div class="item ">
+                  <div id="car_capt" class="carousel-caption">
+                    <p class="name"><span>Константин и Мария</span>Свадебный клип</p>
                 </div>
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-              <span></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-              <span></span>
-            </a>
+                <div class="video_boxesss">
+                  <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/204218501?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+              </div>
           </div>
+          <div class="item ">
+              <div id="car_capt" class="carousel-caption">
+                <p class="name"><span>Денис и Мария</span>Свадебный клип</p>
+            </div>
+            <div class="video_boxesss">
+              <iframe onclick="_gaq.push(['_trackEvent', 'video1', 'play']);" src="//player.vimeo.com/video/202621362?title=0&amp;byline=0&amp;portrait=0" width="100%" height="242px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+      </div>
   </div>
+  <!-- Controls -->
+  <a  id="controls" class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+      <span></span>
+  </a>
+  <a  id="controls" class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+      <span></span>
+  </a>
+</div>
+</div>
 </div>
