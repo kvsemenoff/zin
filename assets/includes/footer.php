7f9d4cdd3aa04d530286e002fe3ce3fc0c5fe7af
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-04-18 11:23:19 +0300

    footer

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,29 +1,18 @@
 <div id="footer">
-  <div class="content">
-
-
-
-
-<!-- VK Widget -->
-<div id="vk_groups"></div>
-
-
-
-
-    <div class="box">
-      <div class="logo">
-          <iframe src="//widget.instagramm.ru/?imageW=4&imageH=3&thumbnail_size=63&type=0&typetext=ilya_zinoveev&head_show=1&profile_show=1&shadow_show=1&bg=255,255,255,1&opacity=true&head_bg=46729b&subscribe_bg=ad4141&border_color=c3c3c3&head_title=" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:300px;height:364px;"></iframe>
-
-      </div>
-      <div class="phone_box">
-        <p class="phone"><span><a href="tel:+7 926 453-95-79"><b>8 (926) 453-95-79 </b</a></span></a></p>
-        <p class="text"><b>Zinoveev@yandex.ru</b></p>
-        <a data-toggle="modal" href="#myModal0"><small>Закажите обратный звонок</small></a></br></br>
-                  <a data-toggle="modal" href="#myModal2" class="politika">Политика конфиденциальности </a>
-      </div>
-    </div>
-    <div class="clear"></div>
+<div class="container">
+  <!-- VK Widget -->
+  <div id="vk_groups" class="col-md-4 vk-footer"></div>
+  <div class="insta col-md-4">
+          <iframe src="//widget.instagramm.ru/?imageW=4&imageH=3&thumbnail_size=63&type=0&typetext=ilya_zinoveev&head_show=1&profile_show=1&shadow_show=1&bg=255,255,255,1&opacity=true&head_bg=46729b&subscribe_bg=ad4141&border_color=c3c3c3&head_title=" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:245px;height:364px;"></iframe>
   </div>
-  <div class="content">
-      </div>
+  <div class="logo col-md-2"></div>
+  <div class="phone_box col-md-2 col-xs-12">
+      <p class="phone"><span><a href="tel:+7 926 453-95-79"><b>8 (926) 453-95-79</b></a></span></a></p>
+      <p class="text"><b>Zinoveev@yandex.ru</b></p>
+      <a data-toggle="modal" href="#myModal0"><small>Закажите обратный звонок</small></a><br><br>
+      <a data-toggle="modal" href="#myModal2" class="politika">Политика конфиденциальности </a>
+  </div>
+      <div class="clear"></div>
+  </div>
+</div>
 </div>
